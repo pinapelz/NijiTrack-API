274e2cb39841b71c1f1cc9e94b94c75f0ebed539
Pinapelz <yukais@pinapelz.com>
2024-10-04 11:57:29 -0700

    add logic to cut log file by half if exceeding 1000kb in 1 run - should not ever happen but this is a failsafe

--- logger.py
+++ logger.py
@@ -1,6 +1,7 @@
 import time
 from datetime import datetime
 import pytz
+import os
 
 def _get_datetime_string():
     utc_now = datetime.now(pytz.timezone('UTC'))
@@ -30,4 +31,14 @@ class Logger:
     
     def log(self, message: str):
         with open(self.path, "a") as file:
+            self.check_log_size()
             file.write(f"[{_get_datetime_string()}] {message}\n")
+    
+    def check_log_size(self):
+        if os.path.getsize(self.path) > self.max_size_bytes:
+            with open(self.path, "r") as file:
+                lines = file.readlines()
+            with open(self.path, "w") as file:
+                file.writelines(lines[int(len(lines) / 2):])
+                file.write("Removed half of the log file due to size\n")
+
